7efdcfbeaf914919420851328d27a3e5b9588849
liujf <cnliujianfei@163.com>
2017-08-29 12:11:59 +0800

    encode decode add .

--- www/secret.js
+++ www/secret.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-    encode: function (arg0, succedoes, error) {
+    encode: function (arg0, success, error) {
         exec(success, error, "BleSecretPlugin", "encode", [arg0]);
     },
     decode: function (arg0, success, error) {
