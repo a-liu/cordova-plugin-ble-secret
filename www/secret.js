ca372a4b85271f7b14c2a8df9eed4d3ac85e934c
liujf <cnliujianfei@163.com>
2017-08-29 14:29:03 +0800

    exports modified

--- www/secret.js
+++ www/secret.js
@@ -1,11 +1,17 @@
 var exec = require('cordova/exec');
-
-module.exports = {
-    encode: function (arg0, success, error) {
-        exec(success, error, "BleSecret", "encode", [arg0]);
-    },
-    decode: function (arg0, success, error) {
-        exec(success, error, "BleSecret", "decode", [arg0]);
-    }
+exports.encode = function (arg0, success, error) {
+    exec(success, error, "BleSecret", "encode", [arg0]);
 }
+
+exports.decode = function (arg0, success, error) {
+    exec(success, error, "BleSecret", "decode", [arg0]);
+}
+// module.exports = {
+//     encode: function (arg0, success, error) {
+//         exec(success, error, "BleSecret", "encode", [arg0]);
+//     },
+//     decode: function (arg0, success, error) {
+//         exec(success, error, "BleSecret", "decode", [arg0]);
+//     }
+// }
 
